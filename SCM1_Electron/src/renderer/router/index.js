44ee24052462901048483a7f05d874b5c972c037
Megumi Kurihara <pomme.c5.musique@gmail.com>
2017-11-10 22:10:51 +0900

    Add electron-vue project

---
+++ SCM1_Electron/src/renderer/router/index.js
@@ -0,0 +1,35 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Auth from '@/store/modules/auth'
+
+Vue.use(Router)
+
+var router = new Router({
+  routes: [   
+    {
+      path: '/',
+      name: 'login',
+      component: require('@/components/Login').default
+    },
+    {
+      path: '/chart',
+      name: 'chart',
+      component: require('@/components/Chart'),
+      meta: {
+        requiresAuth: true
+      }
+    }
+  ]
+})
+
+// router.beforeEach((to, from, next) => {
+//   if (to.matched.some(record => record.meta.requiresAuth) && !Auth.state.loggedIn) {
+//     next({
+//       path: '/login'
+//     })
+//   } else {
+//     next()
+//   }
+// })
+
+export default router
